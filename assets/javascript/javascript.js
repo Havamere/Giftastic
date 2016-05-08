67e01bef3f681e297dbbd01286bcb3eb6b1445db
Kyle Rinehart <Kyle Rinehart>
2016-05-08 00:48:59 -0400

    JS works to the point of adding a button, producung static images, and producing a semi random set of images, may consider setting a for loop to run call 25 times.  Can't do anything after button added and can't animate images yet

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -28,7 +28,7 @@ $('#addButton').on('click', function() {
 	return false;
 })
 //ajax call to produce 25 random gifs of button choice
-function displayGIFS() {
+$('.button').on('click', function(){
 	//random number to set offset parameter of API search so user gets different sets of images
 	var randomNumber = Math.floor(Math.random()*50);
 	//gets name to place into URL API call
@@ -40,52 +40,39 @@ function displayGIFS() {
 		url: queryURL,
 		method: 'GET'
 	})
-		.done(function(resopnse){
+		.done(function(response){
 			//for testing and info gathering
 			console.log(response);
 			//array for objects returned by response
 			var results = response.data;
 			//iterates over each image object in response
-			for (var i=0; i<results.length) {
+			for (var i=0; i<results.length; i++) {
 				// builds img data source to display and control gif
 				var imageDiv = $('<div>');
 				var p = $("<p>").text("Rating: "+results[i].rating);
 				var image = $("<img>");
-			    image.attr("src", results[i].images.fixed_height.url);
-			    image.attr("data-still", results[i].images.original_still.url);
-			    image.attr("data-animate", results[i].images.original.url);
+			    image.attr("src", results[i].images.fixed_width_still.url);
+			    image.attr("data-still", results[i].images.fixed_width_still.url);
+			    image.attr("data-animate", results[i].images.fixed_width.url);
 			    image.attr("data-state", "still");
 			    image.addClass("image");
 			    imageDiv.append(p);
 			    imageDiv.append(image);
 				$('#display').append(imageDiv);
 			}
-			$('.image')on('click', function(){
-				
-				//captures current state of image
-				var state = $(this).attr("data-state");
-		        //flips image animation on and off
-	            if (state == 'still') {
-	                $(this).attr("src", $(this).data('animate'));
-	                $(this).attr('data-state', 'animate');
-	            }
-	            if (state == 'animate') {
-	                $(this).attr("src", $(this).data('still'));
-	                $(this).attr('data-still', 'still');
-	            }
-			})
 		})
-}
-
-
-	// $('.animalImage').on('click', function(){
-//           var state = $(this).attr("data-state");
-//           if (state == 'still') {
-//               $(this).attr("src", $(this).data('animate'));
-//               $(this).attr('data-state', 'animate');
-//           }
-//           if (state == 'animate') {
-//               $(this).attr("src", $(this).data('still'));
-//               $(this).attr('data-still', 'still');
-//           }
-//    });
+})
+//turns animation on and off when image is clicked on
+$('.image').on('click', function(){
+	//captures current state of image
+	var state = $(this).attr("data-state");
+	//flips image animation on and off
+	if (state == 'still') {
+        $(this).attr("src", $(this).data('animate'));
+        $(this).attr('data-state', 'animate');
+    }
+    if (state == 'animate') {
+        $(this).attr("src", $(this).data('still'));
+        $(this).attr('data-still', 'still');
+    }
+})

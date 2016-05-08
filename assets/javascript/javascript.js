a3a63eb849ff7d00631199de24d02f509240e808
Kyle Rinehart <Kyle Rinehart>
2016-05-07 20:37:40 -0400

    JS started, divs set up in html for buttons, view area, and button maker

---
+++ assets/javascript/javascript.js
@@ -0,0 +1,6 @@
+// beginning set of buttons array
+var buttons = ['Luke Skywalker', 'Leia Organa', 'Han Solo', 'Darth Vader', 'Boba Fett', 'Greedo', 'Anakin Skywalker', 'Darth Sidious', 'Mace Windu', 'Obi Wan Kenobi', 'Yoda', 'Stormtroopers', 'Chewbacca', 'Ewoks', 'Darth Maul']
+
+function createButtonSet() {
+
+}

1059263de8cf13a64705ada6463fb84183b0739b
Kyle Rinehart <Kyle Rinehart>
2016-05-11 18:18:29 -0400

    Pushing any updates

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,4 +1,4 @@
-
+ 
 $(document).ready(function(){
 	// beginning set of buttons array
 	var buttons = ['Luke Skywalker', 'Leia Organa', 'Han Solo', 'Darth Vader', 'Boba Fett', 'Greedo', 'Anakin Skywalker', 'Darth Sidious', 'Mace Windu', 'Obi Wan Kenobi', 'Yoda', 'Stormtroopers', 'Chewbacca', 'Ewoks', 'Darth Maul']
